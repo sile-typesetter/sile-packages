1d900ce5dc5820c003d1e986e1931f5c6442b57d
Simon Cozens <simon@simon-cozens.org>
2018-10-02 12:43:25 +0100

    That's more like it

--- packages.lua
+++ packages.lua
@@ -1,6 +1,9 @@
-SILE.Packages = {
-  "fontproof" = {
+Catalogue = {
+  fontproof = {
     description = "A font design testing class",
-    repository  = "https://github.com/silnrsi/fontproof"
+    repository  = "https://github.com/silnrsi/fontproof",
+    version = "v1.0.4"
   }
 }
+
+return Catalogue
