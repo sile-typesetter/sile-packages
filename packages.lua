66137de34903c5e7ceec3fe2b72e5c0739dcc99b
Simon Cozens <simon@simon-cozens.org>
2019-02-06 12:26:40 +0000

    Add endnotes/patchcommand packages

--- packages.lua
+++ packages.lua
@@ -1,8 +1,16 @@
 Catalogue = {
+  endnotes = {
+    description = "Convert footnotes to endnotes",
+    repository  = "https://github.com/simoncozens/sile-endnotes"
+  },
   fontproof = {
     description = "A font design testing class",
     repository  = "https://github.com/silnrsi/fontproof",
     version = "v1.0.4"
+  },
+  patchcommand = {
+    description = "Patch a command with before and after hooks",
+    repository  = "https://github.com/simoncozens/sile-patchcommand"
   }
 }
 
