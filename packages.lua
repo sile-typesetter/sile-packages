9d5fd08656e07248338691b853332c534bf0f203
Simon Cozens <simon@simon-cozens.org>
2019-02-14 11:11:57 +0000

    Update fontproof version; fixes #1

--- packages.lua
+++ packages.lua
@@ -7,7 +7,7 @@ Catalogue = {
   fontproof = {
     description = "A font design testing class",
     repository  = "https://github.com/silnrsi/fontproof",
-    version = "v1.0.4"
+    version = "v1.1.1"
   },
   patchcommand = {
     description = "Patch a command with before and after hooks",
